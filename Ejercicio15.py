790d4f8b6987f2ac61b453ad2d0fbf808cb8c212
renzo <renzodonadio1@gmail.com>
2026-04-27 18:12:06 -0300

    correccion ejer 15

--- Ejercicio15.py
+++ Ejercicio15.py
@@ -4,7 +4,7 @@ def raiz_entera(num1:int) -> int:
 def aux(num1:int, num2:int) -> int:
     if num2 * num2 > num1:
         return num2 - num1
-    return aux(num1, num2 - 1)
+    return aux(num1, num2 + 1)
 
 print(raiz_entera(16))
 print(raiz_entera(25))
