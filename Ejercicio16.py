85b5007552ebf0529604df6a16cc690cc6ffba4b
renzo <renzodonadio1@gmail.com>
2026-04-27 19:52:35 -0300

    ejercicio 16

---
+++ Ejercicio16.py
@@ -0,0 +1,10 @@
+#formula = a[n] = a[n-1] * r
+#a1 = 2
+#r = -3
+
+def termino(num1: int) -> int:
+    if num1 == 0:
+        return 2
+    return termino(num1 - 1) * (-3)
+
+#para mostrar la sucesion
