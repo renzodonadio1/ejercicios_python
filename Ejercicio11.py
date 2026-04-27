9ec44d7285ab675d3a9c632ebd00f993496641a0
renzo <renzodonadio1@gmail.com>
2026-04-27 16:24:29 -0300

    correcion forma recursiva y print

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,4 +1,6 @@
 def invertir_numero(num1:int, numero_invertido=0) -> int:
     if num1 == 0:
         return numero_invertido
-    return invertir_numero(num1 // 10, numero_invertido=numero_invertido * 10 + num1 % 10)
+    return invertir_numero(num1 // 10, numero_invertido * 10 + num1 % 10)
+
+print(invertir_numero(1234)) #4321
