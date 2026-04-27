0cd6b9032aadbd61a1e63b3da8ba6eadc93a5c65
renzo <renzodonadio1@gmail.com>
2026-04-27 20:27:23 -0300

    ejer 20 agrego lista

--- Ejercicio20.py
+++ Ejercicio20.py
@@ -1,4 +1,21 @@
+lista = [1, 2, 3, 4, 5]
+
 def busqueda_centinela(lista, valor, i=0):
     if lista[i] == valor:
         return i
     return busqueda_centinela(lista, valor, i + 1)
+
+def buscar(lista, valor):
+    lista.append(valor)
+
+    pos = busqueda_centinela(lista, valor)
+
+    lista.pop()
+
+    if pos < len(lista):
+        return f"El valor {valor} se encuentra en la posición {pos}"
+    else:
+        return f"El valor {valor} no se encuentra en la lista"
+
+print(buscar(lista, 3))
+print(buscar(lista, 10))
