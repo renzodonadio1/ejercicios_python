8e6ef263801de1a4cd14755f7cc26a23f0c05467
renzo <renzodonadio1@gmail.com>
2026-04-27 16:20:37 -0300

    correccion invertir numero

--- Ejercicio11.py
+++ Ejercicio11.py
@@ -1,2 +1,4 @@
-def invertir_numero(num1:int) -> int:
+def invertir_numero(num1:int, numero_invertido=0) -> int:
     if num1 == 0:
+        return numero_invertido
+    return invertir_numero(num1 // 10, numero_invertido=numero_invertido * 10 + num1 % 10)
