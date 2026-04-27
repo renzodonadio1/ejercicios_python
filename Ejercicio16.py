15ffe55120d09f4212827cf42629d54abf9c0d0b
renzo <renzodonadio1@gmail.com>
2026-04-27 19:56:16 -0300

    correccion ejer 16

--- Ejercicio16.py
+++ Ejercicio16.py
@@ -3,10 +3,16 @@
 #r = -3
 
 def termino(num1: int) -> int:
-    if num1 == 0:
+    if num1 == 1:
         return 2
     return termino(num1 - 1) * (-3)
 
 #para mostrar la sucesion
 
-def mostrar_sucesion(num1: int)
+def mostrar_sucesion(num1: int, i=1):
+    if i > num1:
+        return
+    print(termino(i))
+    mostrar_sucesion(num1, i + 1)
+
+mostrar_sucesion(5)
