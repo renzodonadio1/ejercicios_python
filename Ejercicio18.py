2a4ea56a26f5c244258c2bb63ebb941a32c65e7a
renzo <renzodonadio1@gmail.com>
2026-04-27 20:06:29 -0300

    lineas afuera de la funcion y correcion

--- Ejercicio18.py
+++ Ejercicio18.py
@@ -1,10 +1,19 @@
+matrizA = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
 def recorrer_matriz(matrizA, i=0, j=0):
     if i >= len(matrizA):
         return
     
     if j >= len(matrizA[i]):
+        print() #salto de linea
         recorrer_matriz(matrizA, i + 1, 0)
         return
      
-print(matrizA[i][j])
-recorrer_matriz(matrizA, i, j + 1)
+    print(matrizA[i][j], end=' ')
+    recorrer_matriz(matrizA, i, j + 1)
+
+recorrer_matriz(matrizA)
