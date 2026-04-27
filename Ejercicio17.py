c6fd13d70f1329d01bd150475a9d701112bf60df
renzo <renzodonadio1@gmail.com>
2026-04-27 18:10:01 -0300

    ejer 17

--- Ejercicio17.py
+++ Ejercicio17.py
@@ -1,1 +1,9 @@
-def 
+lista = [1, 2, 3, 4, 5]
+
+def vector_inverso(vector, i):
+    if i < 0:
+        return
+    print(vector[i])
+    vector_inverso(vector, i - 1)
+
+print(vector_inverso(lista))
