145c03e8b0e75c865e5d16b756495b2ee0713866
renzo <renzodonadio1@gmail.com>
2026-05-18 19:31:47 -0300

    ejer 20 terminado

--- TP_N2/Ejercicio20_tp2.py
+++ TP_N2/Ejercicio20_tp2.py
@@ -10,3 +10,40 @@ vehiculo = {
     "camion": 71,
     "colectivo" : 64    
 }
+
+for i in range(30):
+    tipo = random.choice(list(vehiculo.keys()))
+    cabina = random.choice([cabina, cabina2, cabina3])
+
+    cabina.append(tipo)
+
+recaudacion = [0, 0, 0]
+
+conteo = [
+    {"auto": 0, "camioneta": 0, "camion": 0, "colectivo": 0},
+    {"auto": 0, "camioneta": 0, "camion": 0, "colectivo": 0},
+    {"auto": 0, "camioneta": 0, "camion": 0, "colectivo": 0}
+]
+
+cabinas = [cabina, cabina2, cabina3]
+
+for i in range(3):
+    while cabinas[i]:
+        vehiculo_tipo = cabinas[i].pop()
+        recaudacion[i] += vehiculo[vehiculo_tipo]
+        conteo[i][vehiculo_tipo] += 1
+
+for i in range(3):
+     print(f"Cabina {i+1}: ${recaudacion[i]}")
+
+
+     
+mayor = max(recaudacion)
+pos = recaudacion.index(mayor)
+print(f"la cabina {pos+1} recaudo mas: ${mayor}")
+
+for i in range(3):
+    print(f"\nCabina {i+1}:")
+
+    for tipo in conteo[i]:
+        print(f"{tipo}: {conteo[i][tipo]}")
